270b70e4d8db3bd5c36adcefbacfe5738e718c8f
shedupte <33477021+shedupte@users.noreply.github.com>
2019-02-02 14:03:12 -0500

    routes added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+//var data_service= require("data-service");
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -13,13 +14,33 @@ app.use(express.static('public'));
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
-   res.sendFile(path.join(__dirname, "/views/home.html"));
+   res.sendFile(path.join(__dirname, "views/home.html"));
 });
+
+
 
 // setup another route to listen on /about
 app.get("/about", function(req,res){
-   res.sendFile(path.join(__dirname, "/views/about.html"));
+   res.sendFile(path.join(__dirname, "views/about.html"));
 });
+
+// setup another route to listen on /about
+app.get("/employees", function(req,res){
+   res.send("this string for emp"); // employees
+});
+
+// setup another route to listen on /about
+app.get("/managers", function(req,res){
+   res.send("this strin for manager");// managers
+});
+
+// setup another route to listen on /about
+app.get("/departments", function(req,res){
+   res.send("this is a deparment string");//departments
+});
+
+// add response for no matching routes
+
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
